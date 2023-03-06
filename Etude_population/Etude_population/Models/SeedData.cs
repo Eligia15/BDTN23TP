21e4d45b3641311360f28cc8a80a2b5bb7cd57a8
Eligia15 <elisabeth.ngue@groupe-esigelec.org>
2023-03-06 17:20:49 +0100

    Classe population ne fonctionne pas

--- Etude_population/Etude_population/Models/SeedData.cs
+++ Etude_population/Etude_population/Models/SeedData.cs
@@ -7,7 +7,8 @@ namespace Etude_population.Models;
 public static class SeedData // Ajout d’une nouvelle classe SeedData dans Models pour créer la base et ajouter un film si besoin
 {
     public static void Initialize(IServiceProvider serviceProvider)
-    {
+    {   
+
         using (var context = new Etude_populationContext(
         serviceProvider.GetRequiredService<
         DbContextOptions<Etude_populationContext>>()))
@@ -19,31 +20,32 @@ public static class SeedData // Ajout d’une nouvelle classe SeedData dans Mode
                 return; // On ne fait rien
             }
 
-            // S’il y a déjà une population dans la base
-            if (context.Population.Any())
-            {
-                return; // On ne fait rien
-            }
 
             // Sinon on en ajoute un
             context.Pays.AddRange(
             new Pays
             {
                 Name = "Cameroun",
-                Continent= "Afrique"
+                Continent = "Afrique"
             }
             );
+
+
+            if (context.Population.Any())
+            {
+                return; // On ne fait rien
+            }
 
             context.Population.AddRange(
             new Population
             {
                 Nombre = 7000000,
-                Pays_app = Cameroun,
                 Annee = 2020
             }
             );
             context.SaveChanges();
         }
+
     }
 }
 
