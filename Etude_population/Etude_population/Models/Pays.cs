c0467eab405c0be85076cdfb96a83ab2048904c5
Eligia15 <126483516+Eligia15@users.noreply.github.com>
2023-04-01 11:36:56 +0200

    Delete Etude_population directory

--- Etude_population/Etude_population/Models/Pays.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Etude_population.Models
-{
-    public class Pays
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Continent { get; set; }
-    }
-}
