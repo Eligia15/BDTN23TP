f457a26d8517c76a27717e2073aceb949a2f632a
Tobieeldou <tobie.eloundou@groupe-esigelec.org>
2023-04-04 18:43:58 +0200

    Ajout de la liste des pays dans un continent

--- projet_pop/projet_pop/Models/Continent.cs
+++ projet_pop/projet_pop/Models/Continent.cs
@@ -5,7 +5,7 @@
         
         public int Id { get; set; }
         public string Name { get; set; }
-        public ICollection<Pays> pays { get; set; }
+        public ICollection<Pays> pays { get; set; } //liste des pays dans un continent
     }
 }
 
